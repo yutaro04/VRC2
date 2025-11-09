c67eb591d88e5963ed3c1b2371dbbd9b4869fc9b
kokomme <116422742+KomedaMasaya@users.noreply.github.com>
2025-11-09 19:27:15 +0900

    20251109 機能別コンポーネントのディレクトリ構成を整理し、各Screenファイルを追加（開発下準備）

---
+++ src/components/features/events/EventDetailScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EventDetailScreen: React.FC = () => {
+  return (
+    <div>
+      <h1>イベント詳細</h1>
+      {/* イベント詳細情報をここに表示 */}
+    </div>
+  );
+};
+
+export default EventDetailScreen;
